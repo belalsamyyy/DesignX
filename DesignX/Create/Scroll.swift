459cbee8a7e2ffd2dcb239fd59342ecd09e3865f
Belal Samy <belalsamy10@gmail.com>
2021-11-08 16:02:20 +0200

    Updated DesignX.pdspec to version 1.1.0

--- DesignX/Create/Scroll.swift
+++ DesignX/Create/Scroll.swift
@@ -12,10 +12,15 @@ public extension UIScrollView {
     
     func create(container: UIView) {
         // create dynamic scrollview in one line
+
         self.addSubview(container)
         self.layout(XW: .leadingAndCenter(nil, 0), YH: .TopAndBottom(nil, 0, nil, 0))
-        container.layout(XW: .leadingAndCenter(nil, 0), YH: .TopAndBottom(nil, 0, nil, 0))
-    
+        container.layout(XW: .leadingAndTrailingAndWidth(nil, 0, nil, 0, .equal(nil, 1)), YH: .TopAndBottom(nil, 0, nil, 0))
+        
+        // to get rid of top safearea margin
+        //let topPadding = UIApplication.shared.windows.first?.safeAreaInsets.top ?? 0
+        //container.constraint(Y: .top(nil, -topPadding))
+        
         self.autoresizingMask = .flexibleHeight
         self.bounces = true
         self.showsHorizontalScrollIndicator = true
